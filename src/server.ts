ad657f8910ba4327f5240b5e0a736212ce382428
anjuvinay <aanjups88@gmail.com>
2025-01-26 00:31:54 +0530

    again                                                 changed the url of frontend

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 8000;
 app.use(
   cors({
     credentials: true,
-    origin: "https://pic-stack-4bfq.vercel.app/",
+    origin: "https://pic-stack-4bfq.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   })
 );
